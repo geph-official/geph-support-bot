23d3de625b17595079564befbff2f19aaefe5525
thisbefruit <thisbefruit@protonmail.com>
2023-05-22 21:50:55 -0500

    fixed shellscript

--- start-latest-bot.sh
+++ start-latest-bot.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
+export PATH=$PATH:/root/.cargo/bin
 
-bot_path='/root/geph-support-bot/';
-config_path='/root/geph-support-bot/config.yaml';
+bot_path='/root/geph-support-bot/'
+config_path='/root/geph-support-bot/config.yaml'
 # bot_path='/home/thisbefruit/GEPH4/geph-support-bot/';
 # config_path='/home/thisbefruit/GEPH4/geph-support-bot/config.yaml';
 
-cd $bot_path;
-git pull;
+cd $bot_path
+git pull
 RUST_LOG=geph_support_bot cargo run -- -c $config_path
