f28bfba054533b0d8429e5352c6c172a0734288a
thisbefruit <thisbefruit@protonmail.com>
2023-05-25 20:39:30 -0500

    email wip

--- src/email.rs
+++ src/email.rs
@@ -1,12 +1,19 @@
+use std::collections::HashMap;
+
 use async_compat::CompatExt;
 use warp::Filter;
 
 pub async fn handle_email() -> anyhow::Result<()> {
-    let hello_world = warp::path("support-bot-email").map(|| "Hello, World!");
-    warp::serve(hello_world)
-        .run(([0, 0, 0, 0], 3030))
-        .compat()
-        .await;
+    let echo = warp::path("support-bot-email")
+        .and(warp::post())
+        .and(warp::body::form())
+        .map(|form: HashMap<String, String>| {
+            let form = format!("{:?}", form);
+            log::debug!("{}", form);
+            form
+        });
+
+    warp::serve(echo).run(([0, 0, 0, 0], 3030)).compat().await;
     // receives emails
 
     // gets response & acts
