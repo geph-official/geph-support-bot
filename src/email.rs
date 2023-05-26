774a236155575d3bba8d35bf62b3a116967e0b7a
thisbefruit <thisbefruit@protonmail.com>
2023-05-25 21:19:47 -0500

    debugging

--- src/email.rs
+++ src/email.rs
@@ -4,19 +4,21 @@ use async_compat::CompatExt;
 use warp::Filter;
 
 pub async fn handle_email() -> anyhow::Result<()> {
-    let echo = warp::path("support-bot-email")
-        .and(warp::post())
-        .and(warp::body::form())
-        .map(|form: HashMap<String, String>| {
-            let form = format!("{:?}", form);
-            log::debug!("{}", form);
-            form
-        });
+    let echo = warp::path("support-bot-email").and(warp::post()).map(|| {
+        log::debug!("got email!");
+        "Hi! I am GephSupportBot. How can I help you with Geph today?"
+    });
+    // .and(warp::body::form())
+    // .map(|form: HashMap<String, String>| {
+    //     let form = format!("{:?}", form);
+    //     log::debug!("{}", form);
+    //     form
+    // });
 
     warp::serve(echo).run(([0, 0, 0, 0], 3030)).compat().await;
     // receives emails
 
-    // gets response & acts lol
+    // gets response & acts
 
     // sends reply
     Ok(())
