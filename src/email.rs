f568f60b4f4de20f975eec3b99382277576f4724
thisbefruit <thisbefruit@protonmail.com>
2023-05-27 17:13:38 -0500

    sending email added

--- src/email.rs
+++ src/email.rs
@@ -2,15 +2,25 @@ use std::collections::HashMap;
 
 use anyhow::Context;
 use async_compat::CompatExt;
+use once_cell::sync::Lazy;
+use regex::Regex;
+use reqwest::header;
+use serde_json::json;
+use smol::lock::Semaphore;
 use warp::Filter;
 
-use crate::{responder::respond, Message};
+use crate::{
+    database::{Platform, Role},
+    responder::respond,
+    Message, CONFIG, DB,
+};
 
 #[derive(Debug)]
 struct ParsedEmail {
     title: String,
     body: String,
-    sender: String,
+    sender_name: String,
+    sender_email: String,
     message_id: String,
 }
 
@@ -30,29 +40,49 @@ pub async fn handle_email() -> () {
 }
 
 async fn handle_email_inner(email: HashMap<String, String>) -> anyhow::Result<String> {
-    let parsed_email = parse_email(email);
+    let parsed_email = parse_email(email)?;
     log::debug!(
-        "title: {}\nbody: {}\nsender: {}\nmessage_id: {}",
+        "title: {}\nbody: {}\nsender_name: {}\nsender_emal: {}\nmessage_id: {}",
         parsed_email.title,
         parsed_email.body,
-        parsed_email.sender,
+        parsed_email.sender_name,
+        parsed_email.sender_email,
         parsed_email.message_id
     );
 
-    Ok("lol".to_owned())
-    // let msg = Message {
-    //     text: todo!(),     // text = title + email body
-    //     convo_id: todo!(), // convo_id = sender email address
-    // };
+    let msg = Message {
+        text: parsed_email.title + ": " + &parsed_email.body, // text = title + email body
+        convo_id: get_convo_id(&parsed_email.sender_email).await?, // convo_id = sender email address
+    };
     // let resp = respond(msg.clone())
     //     .await
     //     .context("cannot calculate response")?;
-    // // add question & response to db
+    let resp = "Hi! My name is GephSupportBot. How can I help you today?".to_owned();
+
+    // add question & response to db
+    DB.insert_msg(
+        &msg,
+        Platform::Email,
+        Role::User,
+        json!({ "sender": parsed_email.sender_email }),
+    )
+    .await?;
+    DB.insert_msg(
+        &Message {
+            text: resp.clone(),
+            convo_id: msg.convo_id,
+        },
+        Platform::Email,
+        Role::Assistant,
+        json!({"lol": "todo"}),
+    )
+    .await?;
 
     // send email response
+    Ok(resp)
 }
 
-fn parse_email(email: HashMap<String, String>) -> ParsedEmail {
+fn parse_email(email: HashMap<String, String>) -> anyhow::Result<ParsedEmail> {
     let title = email
         .get("subject")
         .unwrap_or(&"Unknown Subject".to_string())
@@ -70,10 +100,57 @@ fn parse_email(email: HashMap<String, String>) -> ParsedEmail {
         .unwrap_or(&"No Message-Id".to_string())
         .clone();
 
-    ParsedEmail {
+    let re = Regex::new(r"^(?P<name>[^<]+)\s*<(?P<email>[^>]+)>$").unwrap();
+    let captures = re.captures(&sender).context("cannot parse sender")?;
+
+    let sender_name = captures
+        .name("name")
+        .context("no sender name")?
+        .as_str()
+        .trim()
+        .to_string();
+    let sender_email = captures
+        .name("email")
+        .context("no sender email")?
+        .as_str()
+        .trim()
+        .to_string();
+
+    Ok(ParsedEmail {
         title,
         body,
-        sender,
+        sender_name,
+        sender_email,
         message_id,
-    }
+    })
+}
+
+async fn get_convo_id(sender_email: &str) -> anyhow::Result<i64> {
+    // sender is stored in the metadata field of each email conversation
+    todo!()
+}
+
+async fn send_email(subject: &str, body: &str, to: &str, in_reply_to: &str) -> anyhow::Result<()> {
+    static MAILGUN_LIMIT: Lazy<Semaphore> = Lazy::new(|| Semaphore::new(16));
+    let _guard = MAILGUN_LIMIT.acquire().await;
+    log::info!("sending email!");
+    let params = [
+        ("from", "GephSupportBot <support@bot.geph.io>"),
+        ("to", to),
+        ("subject", subject),
+        ("text", body),
+    ];
+    let base64_uname_pwd = base64::encode(format!("api:{}", CONFIG.mailgun_key));
+    let auth_value = format!("Basic {}", base64_uname_pwd);
+
+    let client = reqwest::Client::new();
+    let res = client
+        .post("https://api.eu.mailgun.net/v3/bot.geph.io/messages")
+        .header(header::AUTHORIZATION, auth_value)
+        .form(&params)
+        .send()
+        .compat()
+        .await?;
+    dbg!(res);
+    Ok(())
 }
