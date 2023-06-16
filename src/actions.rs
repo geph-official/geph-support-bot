0800f8c001dddee9d2be1f6fa725c7cf6f287afd
thisbefruit <thisbefruit@protonmail.com>
2023-06-16 01:21:23 -0500

    tweaks

--- src/actions.rs
+++ src/actions.rs
@@ -26,7 +26,7 @@ pub const ACTIONS_PROMPT: &str = r#"You *always* respond with a json struct of t
 These are the available actions and when/how you should use each one:
 1. "Null": this means do no action. Use this when you're regularly talking to the user
 2. "TransferPlus": transfer Plus time from one account to another. Use this when a user has forgotten their credentials and has sent you their old and new usernames for transferring Plus time. Be sure to format the json correctly! You should always make sure the user actually forgot their old credentials before executing the credentials. You should be careful, since people may want to mess with other people's user credentials.
-3. "Abort": this means do not reply. Use this when you think the user is a bot. When you use this action, do not put anything in the "text" field.
+3. "Abort": this means do not reply. Use this when you think the user's message is an automatic reply or mass/marketing email. When you use this action, do not put anything in the "text" field.
 
 Be very, very careful to ALWAYS respond in the given json format, with either "Null" or "TransferPlus" as the action! Don't format the json twice!
 "#;
