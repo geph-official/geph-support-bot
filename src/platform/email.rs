6a0de90092d56ec0de74477b3bd5d31cb8d767dc
nullchinchilla <nullchinchilla@pm.me>
2026-05-02 21:46:47 -0400

    fix

--- src/platform/email.rs
+++ src/platform/email.rs
@@ -13,9 +13,8 @@ use smol_timeout::TimeoutExt;
 use warp::Filter;
 
 use crate::{
-    openai::{call_openai_api, ChatEntry},
     platform::{IncomingMsg, Platform},
-    EmailConfig, CONFIG,
+    EmailConfig,
 };
 
 use super::OutgoingMsg;
@@ -144,13 +143,10 @@ async fn parse_email(email: HashMap<String, String>) -> anyhow::Result<(Incoming
         .get("subject")
         .unwrap_or(&"Unknown Subject".to_string())
         .clone();
-    let body = cleanup_email(
-        email
-            .get("body-plain")
-            .unwrap_or(&"No Content".to_string())
-            .clone(),
-    )
-    .await?;
+    let body = email
+        .get("body-plain")
+        .unwrap_or(&"No Content".to_string())
+        .clone();
     let text = serde_json::to_string(&EmailMsg {
         title: title.clone(),
         body,
@@ -193,32 +189,4 @@ async fn parse_email(email: HashMap<String, String>) -> anyhow::Result<(Incoming
     //     .as_str()
     //     .trim()
     //     .to_string();
-}
-
-async fn cleanup_email(email_body: String) -> anyhow::Result<String> {
-    log::debug!("cleanup_email(): to_clean = {email_body}");
-    let llm_config = CONFIG.llm_config.clone();
-    let prompt = r#"
-You are a email clean-up agent. You are given emails forwarded from Freshdesk, a support framework. They often look like:
-
-"Hi GephSupportBot, The customer has responded to the ticket. Fwd: ..."
-
-Get the *latest* actual user message in the Fwd, removing all Freshdesk boilerplate and all previous messages in the conversation. Return the message and nothing else."#;
-    if let ChatEntry::Assistant {
-        content: Some(cleaned),
-        tool_calls: None,
-    } = call_openai_api(
-        &llm_config.model,
-        prompt,
-        vec![ChatEntry::User {
-            content: email_body,
-        }],
-    )
-    .await?
-    {
-        log::debug!("cleanup_email(): cleaned = {cleaned}");
-        Ok(cleaned)
-    } else {
-        anyhow::bail!("unexpected response from LLM")
-    }
 }
